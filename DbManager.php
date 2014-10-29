c75bbf0b757238d04d8fca1c0775bf8fbf1aea7a
Vladimir Yazykov <neizerth@gmail.com>
2014-10-29 14:53:39 +0300

    Init

--- DbManager.php
+++ DbManager.php
@@ -2,7 +2,19 @@
 
 namespace usefulweb;
 
+use \yii\db\Query;
+
 class DbManager extends yii\rbac\DbManager
 {
-    
+    public function removeUserRole($user_id,$roleName)
+    {
+        $users = is_array($user_id) ? $user_id : [$user_id];
+        $roles = is_array($roleName) ? $roleName : [$roleName];
+        
+        $query = new Query;
+        $query
+        ->createCommand()
+        ->delete($this->assignmentTable,['item_name' => $roleName,'user_id'=>$users])
+        ->execute();
+    }
 }
